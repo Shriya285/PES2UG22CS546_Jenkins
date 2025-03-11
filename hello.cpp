68470837b9bf81caa3ee9813c51897bbe3fc84f1
Shriya285 <shriyakonduru@gmail.com>
2025-03-11 15:31:24 +0530

    Fixed syntax issue and added webhook trigger message

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "Testing Webhook Trigger!" << endl; // <-- Move inside main()
+  return 0;
+}
